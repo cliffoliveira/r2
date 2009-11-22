0c4fb0abf7c2026d11acc4fd24012d37c487b2bd
cliff <cliff@cliff.(none)>
2009-11-22 12:39:13 -0300

    new view menu and capa

---
+++ system/application/views/menu.php
@@ -0,0 +1,44 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+ <ul class="kwicks">
+ <li id="kwick_1">
+   <a href="#" >Home
+       <h3>P&aacute;gina principal</h3>
+  </a>
+ </li>
+ <li id="kwick_2">
+   <a href="#">SCM
+     <h3>Opere legalmente</h3>
+  </a>
+ </li>
+ <li id="kwick_3">
+     <a href="#">Servi&ccedil;os
+     <h3>Consultoria</h3>
+  </a>
+ </li>
+ <li id="kwick_4">
+   <a href="#">Parceiros
+     <h3>Locais</h3>
+  </a>
+ </li>
+ <li id="kwick_5">
+   <a href="#">Cadastre-se
+     <h3>Teste</h3>
+  </a>
+ </li>
+ <li id="kwick_6">
+   <a href="#/">Contato
+     <h3>Fale conosco</h3>
+  </a>
+ </li>
+ <li id="kwick_7">
+   <a href="#">Login
+       <h3>&Aacute;rea restrita</h3>
+  </a>
+ </li>
+ </ul>
