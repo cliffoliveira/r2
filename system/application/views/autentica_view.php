21449e402e03e0a038b121168100feb9ecb4e204
cliff <cliff@cliff.(none)>
2009-11-22 20:58:01 -0300

    comite das views

--- system/application/views/autentica_view.php
+++ system/application/views/autentica_view.php
@@ -5,3 +5,37 @@
  */
 
 ?>
+<fieldset>
+    <legend>&Aacute;rea Restrita</legend>
+
+
+    <table id="tabGeral">
+        <tr>
+            <th>Servi&ccedil;o</th>
+            <th></th>
+            
+
+        </tr>
+        <tr>
+            <td><a href="#">Usu&aacute;rios</a></td>
+            <td>Controle de usu&aacute;rios</td>
+            
+        </tr>
+        <tr>
+            <td><a href="#">Clientes</a></td>
+            <td>Conrole de Clientes</td>
+            
+        </tr>
+        <tr>
+            <td><a href="#">Grupos</a></td>
+            <td></td>
+            
+        </tr>
+        <tr>
+            <td><a href="#">Financeiro</a></td>
+            <td></td>
+            
+        </tr>
+        
+    </table>
+</fieldset>
